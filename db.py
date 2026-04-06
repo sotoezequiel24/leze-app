da73567c7e82cc8eaa3852b6927d76cbc445f875
sotoezequiel24 <sotoezequiel24@gmail.com>
2026-04-05 23:56:34 -0300

    db.py

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 def connect():
     return sqlite3.connect("chat.db")
@@ -10,7 +12,9 @@ def init_db():
     c.execute("""
     CREATE TABLE IF NOT EXISTS users (
         username TEXT PRIMARY KEY,
-        password TEXT
+        password TEXT,
+        status TEXT,
+        last_seen TEXT
     )
     """)
 
@@ -18,35 +22,58 @@ def init_db():
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user TEXT,
-        msg TEXT
+        msg TEXT,
+        type TEXT
     )
     """)
 
-    c.execute("INSERT OR IGNORE INTO users VALUES ('Leo','1234')")
-    c.execute("INSERT OR IGNORE INTO users VALUES ('Eze','1234')")
-
     conn.commit()
     conn.close()
 
-def check_user(user, pwd):
+def create_user(username, password):
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT * FROM users WHERE username=? AND password=?", (user,pwd))
+
+    try:
+        c.execute("INSERT INTO users VALUES (?,?,?,?)",
+        (username, generate_password_hash(password),"En línea",""))
+        conn.commit()
+        return True
+    except:
+        return False
+
+def check_user(username, password):
+    conn = connect()
+    c = conn.cursor()
+
+    c.execute("SELECT password FROM users WHERE username=?", (username,))
     result = c.fetchone()
     conn.close()
-    return result
 
-def save_message(user, msg):
+    if result:
+        return check_password_hash(result[0], password)
+    return False
+
+def save_message(user, msg, type):
     conn = connect()
     c = conn.cursor()
-    c.execute("INSERT INTO messages (user,msg) VALUES (?,?)",(user,msg))
+    c.execute("INSERT INTO messages (user,msg,type) VALUES (?,?,?)",(user,msg,type))
+    conn.commit()
+    msg_id = c.lastrowid
+    conn.close()
+    return msg_id
+
+def update_last_seen(user):
+    conn = connect()
+    c = conn.cursor()
+    c.execute("UPDATE users SET last_seen=? WHERE username=?",
+    (datetime.now().strftime("%H:%M"), user))
     conn.commit()
     conn.close()
 
-def get_messages():
+def set_status(user, status):
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT user, msg FROM messages")
-    data = c.fetchall()
+    c.execute("UPDATE users SET status=? WHERE username=?",(status,user))
+    conn.commit()
     conn.close()
-    return data
